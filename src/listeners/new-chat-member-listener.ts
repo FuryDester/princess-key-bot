bfbe1f430b0b313be518767e79a79b3cfca8c21b
Mikhail Motin <m.motin@qsoft.ru>
2023-10-23 17:25:37 +0300

    Added random number

--- src/listeners/new-chat-member-listener.ts
+++ src/listeners/new-chat-member-listener.ts
@@ -2,6 +2,7 @@ import BaseListener from '@/listeners/base-listener';
 import * as TelegramBot from 'node-telegram-bot-api';
 import Logger from '@/wrappers/logger';
 import { LogTagEnum } from '@/enums';
+import { getRandomNumber } from '@/helpers/get-random-number';
 
 class NewChatMemberListener extends BaseListener {
   getEventTrigger(): RegExp | TelegramBot.MessageType {
@@ -19,9 +20,10 @@ class NewChatMemberListener extends BaseListener {
         continue;
       }
 
+      const number = getRandomNumber(1000000000, 1999999999);
       await bot.sendMessage(
         message.chat.id,
-        `@${user.username}, приветствую! Для регистрации аккаунта введите команду: <b>/reg #ID (Пример: /reg 1014003978)</b>`,
+        `@${user.username}, приветствую! Для регистрации аккаунта введите команду: <b>/reg #ID (Пример: /reg ${number})</b>`,
         { parse_mode: 'HTML' },
       );
       Logger.info(`Sent hello message to chat ${message.chat.id} to user ${user.id} (${user.username})`, LogTagEnum.Handler);
