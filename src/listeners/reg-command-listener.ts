9ed77d6c6b8d3b92e30df8cd71757d95b18a3b5c
Motin <m.motin@qsoft.ru>
2023-12-14 22:40:49 +0300

    Refactor RegCommandListener to handle multiple IDs
    Updated RegCommandListener class in order to process multiple user IDs at once. Adjustments were made to the regex match in getEventTrigger, and the handling duplicated for each ID in handleEvent method. These changes increase efficiency when dealing with multiple user IDs.

--- src/listeners/reg-command-listener.ts
+++ src/listeners/reg-command-listener.ts
@@ -7,7 +7,7 @@ import AccountDto from '@/data-transfer-objects/models/account-dto';
 
 class RegCommandListener extends BaseListener {
   getEventTrigger(): RegExp | TelegramBot.MessageType {
-    return /^\/re[dg] (\d+)/gi;
+    return /^\/re[dg] (.+)/gi;
   }
 
   protected async handleEvent(
@@ -16,53 +16,54 @@ class RegCommandListener extends BaseListener {
     match: RegExpExecArray,
     _metadata: TelegramBot.Metadata,
   ): Promise<void> {
-    const id = Number.parseInt(match[1], 10);
+    const ids = match[1].split(/\D+/).map(Number);
+    for (const currentId of ids) {
+      const accountsModel = new Accounts();
+      const accountsTable = accountsModel.getTable();
+      const result = accountsTable.findOne({
+        chat_id : message.chat.id,
+        id      : currentId,
+      } as object);
 
-    const accountsModel = new Accounts();
-    const accountsTable = accountsModel.getTable();
-    const result = accountsTable.findOne({
-      chat_id : message.chat.id,
-      id      : id,
-    } as object);
+      const totalUsers = accountsTable.count({
+        chat_id: message.chat.id,
+      } as object);
 
-    const totalUsers = accountsTable.count({
-      chat_id: message.chat.id,
-    } as object);
+      const activeUsers = accountsTable.count({
+        chat_id  : message.chat.id,
+        disabled : false,
+      } as object);
 
-    const activeUsers = accountsTable.count({
-      chat_id  : message.chat.id,
-      disabled : false,
-    } as object);
+      if (!result) {
+        const accountDto = new AccountDto();
+        accountDto.activated_promos = [];
+        accountDto.error_promos = [];
+        accountDto.id = currentId;
+        accountDto.disabled = false;
+        accountDto.chat_id = message.chat.id;
 
-    if (!result) {
-      const accountDto = new AccountDto();
-      accountDto.activated_promos = [];
-      accountDto.error_promos = [];
-      accountDto.id = id;
+        accountsTable.insertOne(accountDto);
+
+        await bot.sendMessage(message.chat.id, `Пользователь добавлен! <b>(${activeUsers + 1}/${totalUsers + 1})</b>`, { parse_mode: 'HTML' });
+        Logger.info(`User ${currentId} was added in chat ${message.chat.id} by user ${message.from.id}`, LogTagEnum.Handler);
+
+        return;
+      }
+
+      const accountDto = accountsModel.formDto(result) as AccountDto;
+      if (!accountDto.disabled) {
+        Logger.warning(`User ${message.from.id} tried to add existing user ${currentId} in chat ${message.chat.id}`, LogTagEnum.Handler);
+
+        await bot.sendMessage(message.chat.id, `Пользователь <b>${currentId}</b> уже был добавлен! <b>(${activeUsers}/${totalUsers})</b>`, { parse_mode: 'HTML' });
+        return;
+      }
+
       accountDto.disabled = false;
-      accountDto.chat_id = message.chat.id;
+      accountsTable.update(accountDto);
 
-      accountsTable.insertOne(accountDto);
-
-      await bot.sendMessage(message.chat.id, `Пользователь добавлен! <b>(${activeUsers + 1}/${totalUsers + 1})</b>`, { parse_mode: 'HTML' });
-      Logger.info(`User ${id} was added in chat ${message.chat.id} by user ${message.from.id}`, LogTagEnum.Handler);
-
-      return;
+      await bot.sendMessage(message.chat.id, `Пользователь <b>${currentId}</b> добавлен! <b>(${activeUsers + 1}/${totalUsers})</b>`, { parse_mode: 'HTML' });
+      Logger.info(`User ${currentId} was updated (disabled = false) in chat ${message.chat.id} by user ${message.from.id}`, LogTagEnum.Handler);
     }
-
-    const accountDto = accountsModel.formDto(result) as AccountDto;
-    if (!accountDto.disabled) {
-      Logger.warning(`User ${message.from.id} tried to add existing user ${id} in chat ${message.chat.id}`, LogTagEnum.Handler);
-
-      await bot.sendMessage(message.chat.id, `Пользователь уже был добавлен! <b>(${activeUsers}/${totalUsers})</b>`, { parse_mode: 'HTML' });
-      return;
-    }
-
-    accountDto.disabled = false;
-    accountsTable.update(accountDto);
-
-    await bot.sendMessage(message.chat.id, `Пользователь добавлен! <b>(${activeUsers + 1}/${totalUsers})</b>`, { parse_mode: 'HTML' });
-    Logger.info(`User ${id} was updated (disabled = false) in chat ${message.chat.id} by user ${message.from.id}`, LogTagEnum.Handler);
   }
 
   isOnText(): boolean {
